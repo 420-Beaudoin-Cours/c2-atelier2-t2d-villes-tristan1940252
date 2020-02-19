e58fa7bf316423b5198e9c20a6522770db3a18f2
tristan1940252 <trit0tv0@gmail.com>
2020-02-19 14:19:53 -0500

    Fini

--- Main.java
+++ Main.java
@@ -8,9 +8,12 @@ public class Main {
                 {71,63,70,72,76,90,89,96,89,80,92,92},
                 {87,68,84,84,78,78,95,91,73,89,86,102}};
 
-//     Villes.afficherTab(meteo);
+//        Villes.afficherTab(meteo);
 //        System.out.println(Villes.sommeUneLigne(meteo , 0));
-//        System.out.print(Villes.moyenneUneLigne(meteo, 1));
-
+//        System.out.print(Villes.moyenneUneLigne(meteo, 0));
+//        System.out.print(Villes.moyenneUneCol(meteo, 1));
+//        System.out.println(Villes.maxLigne(meteo,1));
+//        System.out.println(Villes.minLigne(meteo,2));
+        System.out.println(Villes.maxCol(meteo, 1));
     }
 }
--- Villes.java
+++ Villes.java
@@ -18,6 +18,7 @@ public class Villes {
         }
         return total;
     }
+
     public static double moyenneUneLigne(int[][] tab2d, int iLigne){
         double moyenne = 0;
         for (int i = 0; i < tab2d[iLigne].length; i++){
@@ -25,13 +26,50 @@ public class Villes {
             }
         try{moyenne /= tab2d[iLigne].length;}
         catch (ArithmeticException e){
-            moyenne = -1;
+            moyenne = 0;
         }
         return moyenne;
     }
+
     public static double moyenneUneCol(int[][] tab2d, int iCol){
         double moyenne = 0;
-        for (int i = 0; i < tab2d.length; i++)
+        for (int i = 0; i < tab2d.length; i++){
+                moyenne += tab2d[i][iCol];
+        }
+        try{
+            moyenne /= tab2d.length;
+        }
+        catch (ArithmeticException e){
+            moyenne = 0;
+        }
         return  moyenne;
+    }
+
+    public static int maxLigne(int[][] tab2d, int iLigne){
+        int imax = 0;
+        for (int i = 0; i < tab2d[iLigne].length; i++){
+            if (tab2d[iLigne][i] > tab2d[iLigne][imax]){
+                imax = i;
+            }
+        }
+        return tab2d[iLigne][imax];
+    }
+
+    public static int minLigne(int[][] tab2d, int iLigne){
+        int imin = 0;
+        for (int i =0; i < tab2d[iLigne].length; i++){
+            if (tab2d[iLigne][imin] > tab2d[iLigne][i])
+                imin = i;
+        }
+        return tab2d[iLigne][imin];
+    }
+
+    public static  int maxCol(int[][] tab2d, int iCol){
+        int imax = 0;
+        for (int i = 0; i < tab2d.length; i++){
+            if (tab2d[imax][iCol] < tab2d[i][iCol])
+                imax = i;
+        }
+        return tab2d[imax][iCol];
     }
 }
